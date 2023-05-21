df76926a41f7edaf3a044396153e4f32f80b9df8
Daniela <danscarballo@gmail.com>
2023-05-21 20:01:35 -0300

    Caracteristicas de ES6 a traves de ts

---
+++ ES6/let-var.ts
@@ -0,0 +1,14 @@
+(() => {
+    
+    const nombre = 'Daniela';
+
+    const getName = ():void => {
+        console.log('Viejo getName');
+        
+    }
+
+    // getName = () => { console.log('Nuevo getName') };
+    
+    // getName();
+
+})()
--- main.js
+++ main.js
@@ -1,286 +1,53 @@
 "use strict";
-(() => {
-    // Tipos
-    const batman = 'Bruce';
-    const superman = 'Clark';
-    const existe = false;
-    // Tuplas
-    const parejaHeroes = [batman, superman];
-    const villano = ['Lex Lutor', 5, true];
-    // Arreglos
-    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
-    //Enumeraciones
-    let fuerza;
-    (function (fuerza) {
-        fuerza[fuerza["acuaman"] = 0] = "acuaman";
-        fuerza[fuerza["batman"] = 1] = "batman";
-        fuerza[fuerza["flash"] = 5] = "flash";
-        fuerza[fuerza["superman"] = 100] = "superman";
-    })(fuerza || (fuerza = {}));
-    const fuerzaFlash = fuerza.flash;
-    const fuerzaSuperman = fuerza.superman;
-    const fuerzaBatman = fuerza.batman;
-    const fuerzaAcuaman = fuerza.acuaman;
-    // Retorno de funciones
-    function activar_batiseñal() {
-        return 'activada';
-    }
-    function pedir_ayuda() {
-        console.log('Auxilio!!!');
-    }
-    // Aserciones de Tipo
-    const poder = '100';
-    const largoDelPoder = poder.length;
-    console.log(largoDelPoder);
-})();
-(() => {
-    // const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10];
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const villians = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
-    villians.forEach(villian => console.log(villian.toUpperCase()));
-})();
-// tsc
-//tsc --init
-//tsc -watch ESTE
-(() => {
-    let isSuperman = true;
-    let isBatman = false;
-    isSuperman = (isBatman) ? true : false;
-    console.log({ isSuperman });
-})();
-(() => {
-    let AudioLevel;
-    (function (AudioLevel) {
-        AudioLevel[AudioLevel["min"] = 1] = "min";
-        AudioLevel[AudioLevel["medium"] = 2] = "medium";
-        AudioLevel[AudioLevel["max"] = 10] = "max";
-    })(AudioLevel || (AudioLevel = {}));
-    let currentAudio = AudioLevel.max;
-    console.log(currentAudio);
-    console.log(AudioLevel);
-})();
-(() => {
-    const error = (message) => {
-        throw new Error(message);
-    };
-    error('auxilio!');
-})();
-// la funcion never nunca retorna un valor
-// es usada para cuando la funcion lanza un error y no retorna nada
-// o cuando la funcion tiene un bucle infinito
-// y no retorna nada
-// suele terminar con un error
-(() => {
-    let nada = undefined;
-    console.log(nada);
-})();
-(() => {
-    let avengers = 10;
-    console.log(avengers);
-    const villians = 20;
-    if (avengers < villians) {
-        console.log('Estamos en problemas');
-    }
-    else {
-        console.log('Nos salvamos');
-    }
-    avengers = Number('123a');
-    console.log({ avengers });
-})();
-(() => {
-    var _a;
-    const batman = 'Batman';
-    const linternaVerde = "Linterna Verde";
-    const volcanNegro = `Heroe: Volcan Negro`;
-    const abc = 123;
-    console.log(`I'm ${batman}, ${abc.toString()}`);
-    console.log(batman.toUpperCase());
-    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No esta presente');
-})();
-(() => {
-    const hero = ['Dr. Strange', 100, true];
-    hero[0] = 'Ironman';
-    hero[1] = 50;
-    hero[2] = false;
-    console.log(hero);
-})();
-(() => {
-    function callBatman() {
-        return;
-    }
-    const callSuperman = () => {
-        return;
-    };
-    const a = callBatman();
-    console.log(a);
-})();
-// void significa que no hay valor de retorno
-// Funciones Básicas
-function sumar(a, b) {
-    return a + b;
-}
-const contar = (heroes) => {
-    return heroes.length;
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
 };
-const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-contar(superHeroes);
-//Parametros por defecto
-const llamarBatman = (llamar) => {
-    if (llamar) {
-        console.log("Batiseñal activada");
+(() => {
+    const avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr.',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500,
+    };
+    const printAvenger = (_a) => {
+        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
+        console.log(ironman, resto);
+    };
+    const avengersArr = ['Capitán América', true, 150.15];
+    const [capitan, ironman, seriaUnNumero] = avengersArr;
+    console.log({ ironman, capitan });
+})();
+(() => {
+    const ironman = {
+        name: 'Ironman',
+        weapon: 'Armorsuit'
+    };
+    const captainAmerica = {
+        name: 'Captain America',
+        weapon: 'Shield'
+    };
+    const thor = {
+        name: 'Thor',
+        weapon: 'Mjolnir'
+    };
+    const avengers = [ironman, captainAmerica, thor];
+    for (const avenger of avengers) {
+        console.log(avenger.name, avenger.weapon);
     }
-};
-llamarBatman();
-// Rest?
-const unirheroes = (...personas) => {
-    return personas.join(", ");
-};
-// Tipo funcion
-const noHaceNada = (numero, texto, booleano = true, arreglo) => { };
-// Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco;
-noHaceNadaTampoco = noHaceNada;
-(() => {
-    const fullName = (firstName, lastName, upper = false) => {
-        if (upper) {
-            return `${firstName} ${lastName || '---'}`.toUpperCase();
-        }
-        else {
-            return `${firstName} ${lastName || '---'}`;
-        }
-    };
-    const name = fullName('Tony', 'Stark', true);
-    console.log({ name });
 })();
 (() => {
-    const fullName = (firstName, lastName) => {
-        return `${firstName} ${lastName || '--'}`;
+    const nombre = 'Daniela';
+    const getName = () => {
+        console.log('Viejo getName');
     };
-    const name = fullName('Tony');
-    console.log({ name });
-})();
-// lastName? Optional arguments
-(() => {
-    const fullName = (firstName, lastName) => {
-        return `${firstName} ${lastName}`;
-    };
-    let noName;
-    const name = fullName('Tony', 'Stark');
-    console.log({ name });
-})();
-(() => {
-    const fullName = (firstName, ...restArgs) => {
-        return `${firstName} ${restArgs.join(' ')}`;
-    };
-    const superman = fullName('Clark', 'Joseph', 'Kent');
-    console.log(superman);
-})();
-//? Un parametro rest es un arreglo que contiene el resto de parametros enviados como argumentos a la funcion
-(() => {
-    const addNumber = (a, b) => a + b;
-    const greet = (name) => `Hola ${name}`;
-    const saveTheWorld = () => `The world is saved!`;
-    let myFunction;
-    //! myFunction = 10;
-    //! console.log(myFunction);
-    // myFunction = addNumber;
-    // console.log(myFunction(1, 2));
-    //? myFunction = greet;
-    //? console.log(myFunction('Daniela'));
-    myFunction = saveTheWorld;
-    console.log(myFunction());
-})();
-(() => {
-    const hero = 'Flash';
-    function returnName() {
-        return hero;
-    }
-    const activateBatisignal = () => {
-        return 'Batiseñal activada';
-    };
-    console.log(typeof activateBatisignal);
-    const heroName = returnName();
-})();
-// Objetos
-(() => {
-    const batimovil = {
-        carroceria: "Negra",
-        modelo: "6x6",
-        antibalas: true,
-        pasajeros: 4
-    };
-    const bumblebee = {
-        carroceria: "Amarillo con negro",
-        modelo: "4x2",
-        antibalas: true,
-        pasajeros: 4,
-        disparar() {
-            console.log("Disparando");
-        }
-    };
-    const villanos = [{
-            nombre: "Lex Luthor",
-            edad: 54,
-            mutante: false
-        }, {
-            nombre: "Erik Magnus Lehnsherr",
-            edad: 49,
-            mutante: true
-        }, {
-            nombre: "James Logan",
-            edad: undefined,
-            mutante: true
-        }];
-    const charles = {
-        poder: "psiquico",
-        estatura: 1.78
-    };
-    const apocalipsis = {
-        lider: true,
-        miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
-    };
-    // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-    let mystique;
-    mystique = charles;
-    mystique = apocalipsis;
-});
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super velocidad', 'Viajar en el tiempo'],
-    };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super velocidad'],
-    };
-})();
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: [1, 2],
-    };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: [1],
-        getName() {
-            return this.name;
-        }
-    };
-})();
-(() => {
-    let myCustomVariable = 'Daniela';
-    console.log(typeof myCustomVariable);
-    myCustomVariable = 20;
-    console.log(typeof myCustomVariable);
-    myCustomVariable = {
-        name: 'Bruce',
-        age: 43,
-        powers: [1, 2]
-    };
-    console.log(myCustomVariable);
 })();
 //# sourceMappingURL=main.js.map
