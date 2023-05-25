b2cb7c98ec522809e949365ad8d5ad37630a5771
Daniela <danscarballo@gmail.com>
2023-05-25 14:31:10 -0300

    NameSpaces

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,14 @@
+import { Hero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
+// import powers, { Power } from "./data/powers";
 
 
-console.log('Hola Mundo!');
+console.log('Hola Mundo!!!');
 
+const ironman = new Hero('Ironman', 1, 45);
+
+console.log(ironman.power);
 
 
 
